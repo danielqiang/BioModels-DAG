1a2963b9dd3162bd178ffd7a596ac7b6a0801465
qiangd2 <qiangd2@cs.washington.edu>
2019-10-27 19:37:55 -0700

    Refactoring, added docstring

--- BioModels/utils/yield_model_paths.py
+++ BioModels/utils/yield_model_paths.py
@@ -1,5 +1,36 @@
-def yield_model_paths():
-    from . import PROJECT_ROOT
+from pathlib import Path
 
-    for model_path in (PROJECT_ROOT / "BioModels" / "curated").iterdir():
+
+def yield_model_paths(dirpath: Path = None):
+    """
+    Returns a generator over all curated model filepaths.
+    Uses dirpath as the curated model directory if provided;
+    otherwise, assumes the following project structure:
+
+        BioModels
+        |
+        └───BioModels
+            |
+            └───curated
+                |   BIOMD0000000001.xml   # curated model
+                |   ...
+            |   ...
+        |   ...
+
+    :param dirpath: Path to directory containing curated models.
+    :raises FileExistsError if dirpath is invalid (if provided) or the "curated"
+                            directory is not present at "~/BioModels/BioModels/curated".
+    :rtype: generator
+    """
+    if dirpath is None:
+        from . import PROJECT_ROOT
+        dirpath = PROJECT_ROOT / "BioModels" / "curated"
+    if not dirpath.exists():
+        raise FileExistsError("'{}' is not a valid directory path.".format(dirpath))
+
+    for model_path in dirpath.iterdir():
         yield model_path.resolve()
+
+
+for i in yield_model_paths(dirpath=Path("hello")):
+    print(i)
