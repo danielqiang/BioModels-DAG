64fc1fb5e6561f00bab5c7827d174b3f3639e52a
Daniel Qiang <daniel_qiang@hotmail.com>
2019-12-04 00:41:47 -0800

    Updated docstring

--- BioModels/pipeline/bases/baseparser.py
+++ BioModels/pipeline/bases/baseparser.py
@@ -4,18 +4,20 @@ __all__ = ['BaseParser']
 
 
 class BaseParser(ABC):
-    """Base class for BioModels parsers. All parsers must inherit from this class.
-
-    A BioModels parser accepts an SBML file handle and returns an iterator over
-    <Node, Edge, Node> triples. Nodes are dictionaries containing a 'name' key
-    and all hashable values (required by NetworkX). Edges are strings representing
-    edge labels in the NetworkX graph.
+    """Abstract base class for BioModels parsers. All parsers must inherit from this class.
     """
 
     @abstractmethod
     def parser(self, sbml_file, **kwargs):
         """
         Abstract parser method. Must be overridden in concrete subclass.
+
+        A parser() implementation must accept an SBML file handle and
+        return an iterator over <Node, Edge, Node> triples.
+
+        Nodes are dictionaries containing a 'name' key and all hashable values
+        (this is required by NetworkX).
+        Edges are strings representing edge labels in the NetworkX graph.
 
         parser() implementations may accept additional keyword arguments.
         Add keyword arguments to the method signature and pass them to
