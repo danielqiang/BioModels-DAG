ce3f4b9c60778db81cbd75c51bd8749b9fe54084
Daniel Qiang <daniel_qiang@hotmail.com>
2020-01-10 13:14:58 -0800

    Refactoring

--- BioModelsETL/examples/generate_compartment_graph.py
+++ BioModelsETL/examples/generate_compartment_graph.py
@@ -9,18 +9,20 @@ def main():
     filepaths = yield_model_paths()
 
     # Extracted using get_all_go_compartments("curated")
-    all_compartments = {'vesicle', 'glycosome', 'Golgi apparatus', 'endoplasmic reticulum',
-                        'sarcoplasmic reticulum', 'hepatocyte homeostasis', 'cytoplasm',
-                        'early phagosome', 'smooth endoplasmic reticulum', 'endosome lumen',
-                        'extracellular region', 'autophagosome', 'nucleus', 'mitochondrial matrix',
-                        'obsolete intracellular part', 'cell', 'extraorganismal space',
-                        'extracellular membrane-bounded organelle', 'endosome membrane', 'membrane',
-                        'lysosome', 'endosome', 'proteasome complex', 'intracellular',
-                        'cytoplasmic side of plasma membrane', 'endoplasmic reticulum membrane',
-                        'DNA binding', 'cell surface', 'mitochondrion', 'spindle pole body',
-                        'plasma membrane', 'chloroplast stroma', 'Golgi membrane',
-                        'mitochondrial intermembrane space', 'extracellular space',
-                        'nuclear membrane', 'mitochondrial inner membrane', 'cytosol'}
+    all_compartments = {
+        'vesicle', 'glycosome', 'Golgi apparatus', 'endoplasmic reticulum',
+        'sarcoplasmic reticulum', 'hepatocyte homeostasis', 'cytoplasm',
+        'early phagosome', 'smooth endoplasmic reticulum', 'endosome lumen',
+        'extracellular region', 'autophagosome', 'nucleus', 'mitochondrial matrix',
+        'obsolete intracellular part', 'cell', 'extraorganismal space',
+        'extracellular membrane-bounded organelle', 'endosome membrane', 'membrane',
+        'lysosome', 'endosome', 'proteasome complex', 'intracellular',
+        'cytoplasmic side of plasma membrane', 'endoplasmic reticulum membrane',
+        'DNA binding', 'cell surface', 'mitochondrion', 'spindle pole body',
+        'plasma membrane', 'chloroplast stroma', 'Golgi membrane',
+        'mitochondrial intermembrane space', 'extracellular space',
+        'nuclear membrane', 'mitochondrial inner membrane', 'cytosol'
+    }
 
     data = extract_data(filepaths,
                         parser=CompartmentParser(),
